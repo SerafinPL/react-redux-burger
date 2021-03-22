3ce9fe259f5c17ee0cebf8a06df12f71a4b69fc2
serafinpl <ser.burberibar@gmail.com>
2021-03-22 13:46:16 +0100

    reducer of auth so on

--- src/store/reducers/redAuth.js
+++ src/store/reducers/redAuth.js
@@ -9,21 +9,28 @@ const initState = {
 	loading: false
 };
 
+const authSuccess = (state, action) => {
+	updateObject(state, {
+		error: false, 
+		loading: false,
+		token: action.token,
+		userId: action.userId
+	});
+};
+
+const authFail = (state, action) => {
+	updateObject(state, {
+		error: action.error, 
+		loading: false,
+	});
+};
+
 const reducer = (state = initState, action) => {
 
 	switch (action.type){
-		case actionTypes.AUTH_START:
-			return{
-
-			};
-		case actionTypes.AUTH_SUCCESS:
-			return{
-
-			};
-		case actionTypes.AUTH_FAIL:
-			return{
-
-			};
+		case actionTypes.AUTH_START: return updateObject(state, {error: false, loading: true});
+		case actionTypes.AUTH_SUCCESS: return authSuccess(state, action);
+		case actionTypes.AUTH_FAIL: return authFail(state, action);
 		default: return state
 
 	}; // switch
