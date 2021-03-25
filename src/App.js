dbfb6ccc5db833314db78348cbb70c1d397c976b
serafinpl <ser.burberibar@gmail.com>
2021-03-25 17:37:32 +0100

    app.j modyfication

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import OrdersPage from './containers/OrdersPage/OrdersPage';
-import {Route, BrowserRouter, Switch, withRouter} from 'react-router-dom';
+import {Route, BrowserRouter, Switch, withRouter, Redirect} from 'react-router-dom';
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
 
@@ -28,6 +28,7 @@ const App = (props) => {
 			<Switch>
 				<Route path='/auth' component={Auth} />
 	        	<Route path='/' exact component={BurgerBuilder} />
+	        	<Redirect to='/' />
 	        </Switch>
 		);
 
@@ -36,9 +37,9 @@ const App = (props) => {
 			<Switch>
 				<Route path='/orders' exact component={OrdersPage} /> 
 	        	<Route path='/checkout' component={Checkout} />
-	        	<Route path='/auth' component={Auth} />
 	        	<Route path='/logout' component={Logout} />
 	        	<Route path='/' exact component={BurgerBuilder} />
+	        	<Redirect to='/' />
 	        </Switch>
 		);
 	}
